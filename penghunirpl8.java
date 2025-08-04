fdf8fc184a8b39585d61f33d9f5aca95c56523a6
ZianMahdiaaaa <zian_arina_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:23:14 +0700

    Add files via upload

---
+++ penghunirpl8.java
@@ -0,0 +1,42 @@
+public class penghunirpl8 {
+    public static void main(String[] args) {
+
+      //variabel  -> tipe_data nama_variabel = value;
+      String nama1 = "sania mirza";
+      int usia1 = 15;
+      String cita1 = "pengusaha";
+
+      String nama2 = "tazkia affandi";
+      int usia2 = 15;
+      String cita2 = "sukses"; 
+
+
+      String nama3 = "sakti";
+      int usia3 = 15;
+      String cita3 = "sukses";
+
+
+      String nama4 = "Raka";
+      int usia4 = 15;
+      String cita4 = "pengusaha";
+
+
+    String nama5 = "Farhan Haidas";
+    int usia5 = 15;
+    String cita5 = "pengusaha";
+
+
+
+
+    System.out.println(nama1 + usia1 + cita1);
+    System.out.println(nama2 + usia2 + cita2);
+    System.out.println(nama3 + usia3 + cita3);   
+     System.out.println(nama4 + usia4 + cita4);
+    System.out.println(nama5 + usia5  + cita5);
+      
+
+
+
+
+    }
+}
